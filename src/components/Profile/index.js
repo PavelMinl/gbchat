69ba85c8534530e7ae3ff34ed2fc1a357b43df22
PAVEL <mindalev77@mail.ru>
2022-02-14 14:54:28 +0300

    lesson5

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,6 +1,26 @@
+import {useDispatch, useSelector} from "react-redux";
+import {changeShowName} from "../../store/profile/action";
+import './profile.css';
 
+export const Profile = () => {
+const dispatch = useDispatch()
+    const date = useSelector((state) =>state)
 
+    const handleChangeShowName = () => {
+ dispatch(changeShowName)
+    }
 
-export const Profile = () => (
-    <div> ПРОФИЛЬ</div>
-)
+console.log(date);
+
+    return (
+        <>
+    <h3> ПРОФИЛЬ</h3>
+            <div className="profileRed">
+                <input onClick={handleChangeShowName} type="checkbox" id="hello" name="hello" value="yes" />
+                <label htmlFor="hello">Привет</label>
+                {date.showName && <div>HELLO I BOT</div>}
+                <button onClick={handleChangeShowName}>Change show name</button>
+            </div>
+        </>
+    );
+}
