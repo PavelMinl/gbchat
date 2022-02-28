7abfc5e8ea9f8ca9bc65419549d33c624f9ef932
Pavel <mindalev77@mail.ru>
2022-02-28 19:21:45 +0300

    lesson9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {Router} from "./components/Router";
 import {Provider} from "react-redux";
-import {store} from "./store";
+import {persistor, store} from "./store";
+import {PersistGate} from "redux-persist/integration/react";
 
 const App = () => (
     <Provider store={store}>
-    <Router />
-        </Provider>
+        <PersistGate persistor={persistor }>
+            <Router />
+        </PersistGate>
+    </Provider>
     );
 
 export default App;
--- src/utils/constans.js
+++ src/utils/constans.js
@@ -2,3 +2,11 @@ export const AUTHORS = {
     I: "Pavel",
     BOT: "BOT"
 }
+export const apiUrl = "https://api.spaceflightnewsapi.net/v3/artices";
+
+export const FETCH_STATUSES = {
+    IDLE: 0,
+    REQUEST: 1,
+    SUCCESS: 2,
+    FAILURE: 3,
+};
