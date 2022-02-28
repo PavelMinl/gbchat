ff3abcb656d9e8074f99fe21eda9ed696cd1764f
Pavel <mindalev77@mail.ru>
2022-02-28 19:21:45 +0300

    lesson9

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,28 @@
-import { createStore, combineReducers } from "redux";
+import {createStore, combineReducers, applyMiddleware} from "redux";
 import { profileReducer } from "./profile/reducer";
 import { chatsReducer } from "./chats/reducer";
 import {persistReducer, persistStore} from "redux-persist";
-
+import {messagesReducer} from "./messages/reducer";
+import {articlesReducer} from "./articles/reducer";
+import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
     profile: profileReducer,
     chats: chatsReducer,
-    messages: messagesReducer,
-    articles: articlesReducer,
+    messages: messagesReducer ,
+    articles: articlesReducer ,
 });
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const persistConfig = {
     key: "gbMessenger",
-    storage,
+    storage ,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = createStore(
     persistedReducer,
-    composeEnhancers(applyMiddleware(thunk))
+    composeEnhancers(applyMiddleware (thunk))
 );
 export const persistor = persistStore(store);
